56a40feaac7da673002e46c916d53f8f6dc072e0
Víctor <victor@shuttlecloud.com>
2018-03-14 11:41:03 +0100

    Add requests assertions

---
+++ tests/requests/requests_log.test.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const Wendigo = require('../../lib/wendigo');
+const assert = require('assert');
+const configUrls = require('../config.json').urls;
+
+describe("Requests Log", function() {
+    this.timeout(5000);
+    let browser;
+
+    before(async () => {
+        browser = await Wendigo.createBrowser();
+    });
+
+    after(async() => {
+        await browser.close();
+    });
+
+    it("Requests Are Intercepted", async () => {
+        await browser.open(configUrls.requests);
+        // html, css
+        assert.strictEqual(await browser.requests.all.length, 2);
+        await browser.clickText("click me");
+        await browser.wait(10);
+        assert.strictEqual(await browser.requests.all.length, 3);
+        await browser.clickText("click me");
+        await browser.wait(10);
+        assert.strictEqual(await browser.requests.all.length, 4);
+    });
+});
