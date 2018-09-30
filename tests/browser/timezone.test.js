fa72a443a97e53f442d967c62feb9eee1c7412e2
Andrés <angrykoala@outlook.es>
2018-09-30 13:46:20 +0200

    WIP tests on #204

---
+++ tests/browser/timezone.test.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const assert = require('assert');
+const Wendigo = require('../../lib/wendigo');
+const configUrls = require('../config.json').urls;
+
+
+// Skipped until #204 fixed
+describe.skip("Timezone", function() {
+    this.timeout(5000);
+    let browser;
+
+    function getTimezone() {
+        return browser.evaluate(() => {
+            return Intl.DateTimeFormat().resolvedOptions().timeZone; // eslint-disable-line new-cap
+        });
+    }
+
+    after(async() => {
+        await browser.close();
+    });
+
+    it("Change Timezone To UTC", async() => {
+        browser = await Wendigo.createBrowser({
+            timezone: "UTC"
+        });
+
+        await browser.open(configUrls.simple);
+        const tz = await getTimezone();
+        assert.strictEqual(tz, "UTC");
+    });
+
+    it("Change Timezone To Japan", async() => {
+        browser = await Wendigo.createBrowser({
+            timezone: "Japan"
+        });
+        await browser.open(configUrls.simple, {
+            timezone: "Japan"
+        });
+        const tz = await getTimezone();
+        assert.strictEqual(tz, "Japan");
+    });
+});
