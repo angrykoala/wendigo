f780d641fc1e30036a6dc5890a1c01547028a96d
angrykoala <angrykoala@outlook.es>
2019-07-16 01:05:32 +0200

    improved typings on compositer module

--- @types/compositer/index.d.ts
+++ @types/compositer/index.d.ts
@@ -1,1 +1,10 @@
-declare module 'compositer';
+type Class<T = unknown> = new (...arguments: any[]) => T;
+
+type ComponentFunction = (...args: Array<any>) => any;
+
+type Component = { [s: string]: any } | ComponentFunction;
+
+declare module 'compositer' {
+    function compose(baseClass: Class, components: Component, ...childParams: Array<any>): Class;
+    export = compose;
+}
