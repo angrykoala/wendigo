fff17aae251e814c54bc40ae25e5afa63f22fe92
angrykoala <angrykoala@outlook.es>
2020-04-29 15:17:30 +0200

    minor fix #3

--- tests/browser/upload.test.js
+++ tests/browser/upload.test.js
@@ -5,7 +5,7 @@ const configUrls = require('../config.json').urls;
 const utils = require('../test_utils');
 const path = require('path');
 
-const filePath = path.join(__filename, '..', 'dummy_file');
+const filePath = path.join(__filename, '..', '..', 'dummy_file');
 
 describe("Upload", function() {
     this.timeout(5000);
@@ -23,15 +23,16 @@ describe("Upload", function() {
         await browser.close();
     });
 
-    it.skip("File Input Set Relative Path", async() => {
-        await browser.uploadFile(".input3", "../dummy_file");
-        await browser.assert.value(".input3", "C:\\fakepath\\dummy_file");
-    });
-
     it("File Input Set Absolute Path", async() => {
         await browser.uploadFile(".input3", filePath);
         await browser.assert.value(".input3", "C:\\fakepath\\dummy_file");
     });
+
+    it("File Input Set Relative Path", async() => {
+        await browser.uploadFile(".input3", "../../dummy_file");
+        await browser.assert.value(".input3", "C:\\fakepath\\dummy_file");
+    });
+
 
     it("File Input Missing Element", async() => {
         await utils.assertThrowsAsync(async() => {
